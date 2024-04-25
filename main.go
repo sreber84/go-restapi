932ac285657d9234baca6296e42d9889433fe09b
Simon Reber <sreber@redhat.com>
2024-04-25 17:42:35 +0200

    Struggling part 2
    rh-pre-commit.version: 2.1.0
    rh-pre-commit.check-secrets: ENABLED

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 
 	// Start the HTTP server
 	port := 8080
-	fmt.Printf("Server is running on I'm failing to follow %d...\n", port)
+	fmt.Printf("Server is running on I'm struggling ... %d...\n", port)
 	http.ListenAndServe(fmt.Sprintf(":%d", port), router)
 }
 
