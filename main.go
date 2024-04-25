89cee99643cdd702a3a81ceaedf4e8a6ea95496c
Simon Reber <sreber@redhat.com>
2024-04-25 17:24:15 +0200

    Changing to Fedora
    rh-pre-commit.version: 2.1.0
    rh-pre-commit.check-secrets: ENABLED

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 
 	// Start the HTTP server
 	port := 8080
-	fmt.Printf("Server is running on FooBar %d...\n", port)
+	fmt.Printf("Server is running on Fedora %d...\n", port)
 	http.ListenAndServe(fmt.Sprintf(":%d", port), router)
 }
 
