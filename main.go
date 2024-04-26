8e0137414587abb54cde4f30f1c1c7feca33371c
Simon Reber <sreber@redhat.com>
2024-04-26 15:03:05 +0200

    Fixing application port
    rh-pre-commit.version: 2.1.0
    rh-pre-commit.check-secrets: ENABLED

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 
 	// Start the HTTP server
 	port := 8080
-	fmt.Printf("Server is running on fun goes on ... %d...\n", port)
+	fmt.Printf("Server is running on port %d...\n", port)
 	http.ListenAndServe(fmt.Sprintf(":%d", port), router)
 }
 
